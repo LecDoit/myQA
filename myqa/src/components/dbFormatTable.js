911b9fe7ef39caca10ae425f6a4230b86678e148
pikimaru <pikajunglist@gmail.com>
2023-04-13 12:28:13 +0200

    function to visualise format of each dashboard has been done

--- myqa/src/components/dbFormatTable.js
+++ myqa/src/components/dbFormatTable.js
@@ -2,11 +2,9 @@ import React, { useState, useEffect, useMemo } from 'react'
 import {formatDashboard} from '../functions/dashboardFormatFunc'
 
 
-
 export const DbFormatTable = ({data}) =>{
 
     const [stateFormatDb,setstateFormatDb] = useState(null)
-
 
     useEffect(()=>{
         if (data){
@@ -18,29 +16,28 @@ export const DbFormatTable = ({data}) =>{
     },[data])
     
 
-    useEffect(()=>{
-        if (stateFormatDb){
-            console.log(stateFormatDb)
-        }
-    },[stateFormatDb])
-
-    let renderTableHeader = (arg,arg2)=>{
-
-        if (arg){
-            let argumentO = arg[arg2][0]
-            console.log(arg)
-            return Object.keys(argumentO).map((obj,i)=>{
-            
-                return (
-                    <div key={obj}>
-                        {obj}
-                    </div>
-                )
-            })
-        }
+    let renderFixedTableHeader =()=>{
+        return (
+            <div className='table--header'>
+                <div>dbname</div>
+                <div>fontWeight</div>
+                <div>fontStyle</div>
+                <div>textDecoration</div>
+                <div>fontFamily</div>
+                <div>fontSize</div>
+                <div>color</div>
+                <div>textAlign</div>
+                <div>backgroundColor</div>
+                <div>borderWidth</div>
+                <div>borderColor</div>
+                <div>borderStyle</div>
+                <div>textOrientation</div>
+                <div>verticalAlign</div>
+                <div>wrap</div>
+            </div>
+ 
+        )
     }
-
-   
 
     let renderTableContent= (arg,arg2)=>{
         if (arg){
@@ -63,26 +60,44 @@ export const DbFormatTable = ({data}) =>{
                     <div>{value.borderWidth}</div>
                     <div>{value.borderColor}</div>
                     <div>{value.borderStyle}</div>
+                    <div>{value.textOrientation}</div>
+                    <div>{value.verticalAlign}</div>
+                    <div>{value.wrap}</div>
                 </div>
             )
         })
     }
     }
+    let RenderTable=()=>{
+        return(
+            <div>
+                <div className='table'>
+                    <div>Dash Title</div>
+                    <div >{renderFixedTableHeader()}</div>
+                    <div >{renderTableContent(stateFormatDb,"dashTitle")}</div>
+                </div>
+                <div className='table'>
+                    <div>Dash Text</div>
+                    <div >{renderFixedTableHeader()}</div>
+                    <div >{renderTableContent(stateFormatDb,"dashText")}</div>
+                </div>
+                <div className='table'>
+                    <div>dashSubTitle</div>
+                    <div >{renderFixedTableHeader()}</div>
+                    <div >{renderTableContent(stateFormatDb,"dashSubTitle")}</div>
+                </div>
+                <div className='table'>
+                    <div>table</div>
+                    <div >{renderFixedTableHeader()}</div>
+                    <div >{renderTableContent(stateFormatDb,"table")}</div>
+                </div>
+            </div>
+            )
+    }
 
-    return(
-    <div>
-        <div className='table'>
-            <div>Dash Title</div>
-            <div className='table--header'>{renderTableHeader(stateFormatDb,"dashTitle")}</div>
-            <div >{renderTableContent(stateFormatDb,"dashTitle")}</div>
-        </div>
-        <div className='table'>
-            <div>Dash Text</div>
-            <div className='table--header'>{renderTableHeader(stateFormatDb,"dashText")}</div>
-            <div >{renderTableContent(stateFormatDb,"dashText")}</div>
-        </div>
-    </div>
-    )
-
+    if (stateFormatDb){
+        return <RenderTable />
+    } else{
+        return <div></div>
+    }
 }
-// need to create dbname in each case
