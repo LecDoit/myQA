64b22e54b05bf186de6911abc449848df9c7223f
pikimaru <pikajunglist@gmail.com>
2023-04-23 22:59:26 +0200

    working on css and html of the wbTable

--- myqa/src/components/wbTable.js
+++ myqa/src/components/wbTable.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react'
+import Chevron from '../chevron.svg'
+
 
 // import {formatWorkbook} from '../functions/workbookFormatFunc'
 
@@ -41,52 +43,59 @@ export const WbTable = (props) =>{
 
         return(
             <div>
-                <div onClick={()=>{setFormatWorkbookClicked(prev=>!prev)}} className='table'>Format Workbook
-                    <div>{formatWorkbook===false ? "+":"-"}</div>
+                <div onClick={()=>{setFormatWorkbookClicked(prev=>!prev)}} className='table'>
+                    <div className='title'>Format Workbook</div>
+                    <img  className={formatWorkbook===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                 </div>
                 <div className={formatWorkbook===true ? 'content show':'content'}> 
 
                     <div>
-                        <div onClick={()=>{setFontsClicked(prev=>!prev)}} className='table--subtitle'>Fonts
-                            <div>{fonts===false ? "+":"-"}</div>
+                        <div onClick={()=>{setFontsClicked(prev=>!prev)}} className='table--subtitle'>
+                            <div className='subtitle'>Fonts</div>
+                            <img  className={fonts===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                         </div>                
                         <div className={fonts===true ? 'content show':'content'}>
 
                             <div>
-                                <div onClick={()=>{setAllClicked(prev=>!prev)}} className='table--sub--subtitle'>All
-                                    <div>{all===false ? "+":"-"}</div>
+                                <div onClick={()=>{setAllClicked(prev=>!prev)}} className='table--sub--subtitle'>
+                                    <div className='subsubtitle'>All</div>
+                                    <img  className={all===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>
                                 <div className={all===true ? 'content show':'content'}>{renderAllTableContent(props.data,"all")}
                                 </div>
                             </div>
                             
                             <div>
-                                <div onClick={()=>{setWorksheetsClicked(prev=>!prev)}} className='table--sub--subtitle'>WorkSheets
-                                    <div>{worksheets===false ? "+":"-"}</div>
+                                <div onClick={()=>{setWorksheetsClicked(prev=>!prev)}} className='table--sub--subtitle'>
+                                    <div className='subsubtitle'>WorkSheets</div>
+                                    <img  className={worksheets===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>
                                 <div className={worksheets===true ? 'content show':'content'}>{renderAllTableContent(props.data,'worksheet')}
                                 </div>   
                             </div>
 
                             <div>
-                                <div onClick={()=>{setTooltips(prev=>!prev)}}className='table--sub--subtitle'>Tooltips
-                                    <div>{tooltips===false ? "+":"-"}</div>
+                                <div onClick={()=>{setTooltips(prev=>!prev)}}className='table--sub--subtitle'>
+                                    <div className='subsubtitle'>Tooltips</div>
+                                    <img  className={tooltips===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>         
                                 <div className={tooltips===true ? 'content show':'content'}>{renderAllTableContent(props.data,'tooltip')}
                                 </div>    
                             </div>  
 
                             <div>
-                                <div onClick={()=>{setWorksheetTitles(prev=>!prev)}}className='table--sub--subtitle'>Worksheet Titles
-                                    <div>{worksheetTitles===false ? "+":"-"}</div>
+                                <div onClick={()=>{setWorksheetTitles(prev=>!prev)}}className='table--sub--subtitle'>
+                                    <div className='subsubtitle'>Worksheet Titles</div>
+                                    <img  className={worksheetTitles===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>
                                 <div className={worksheetTitles===true ? 'content show':'content'}>{renderAllTableContent(props.data,'title')}
                                 </div>
                             </div>
 
                             <div>
-                                <div onClick={()=>{setDashboardTitles(prev=>!prev)}} className='table--sub--subtitle'>Dashboard Titles
-                                    <div>{dashboardTitles===false ? "+":"-"}</div>
+                                <div onClick={()=>{setDashboardTitles(prev=>!prev)}} className='table--sub--subtitle'>
+                                    <div className='subsubtitle'>Dashboard Titles</div>
+                                    <img  className={dashboardTitles===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>
 
                                 <div className={dashboardTitles===true ? 'content show':'content'}>{renderAllTableContent(props.data,'dashTitle')}
@@ -94,8 +103,9 @@ export const WbTable = (props) =>{
                             </div>
 
                             <div>
-                                <div onClick={()=>{setStoryTitle(prev=>!prev)}} className='table--sub--subtitle'>Story Titles
-                                    <div>{storyTitle===false ? "+":"-"}</div>
+                                <div onClick={()=>{setStoryTitle(prev=>!prev)}} className='table--sub--subtitle'>
+                                    <div className='subsubtitle'>Story Titles</div>
+                                    <img  className={storyTitle===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div> 
 
                                 <div className={storyTitle===true ? 'content show':'content'}>{renderAllTableContent(props.data,'storyTitle')}
@@ -106,8 +116,9 @@ export const WbTable = (props) =>{
                     </div>
 
                     <div>
-                        <div onClick={()=>{setLinesClicked(prev=>!prev)}} className='table--subtitle'> Lines
-                            <div>{lines===false ? "+":"-"}</div>
+                        <div onClick={()=>{setLinesClicked(prev=>!prev)}} className='table--subtitle'>
+                            <div className='subtitle'>Lines</div>
+                            <img  className={lines===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                         </div>
 
                         <div className={lines===true ? 'content show':'content'}>
@@ -115,8 +126,8 @@ export const WbTable = (props) =>{
                             <div>
                                 <div onClick={()=>{setGridlineClicked(prev=>!prev)}}
                                     className='table--sub--subtitle'>
-                                    Grid Lines
-                                    <div>{gridline===false ? "+":"-"}</div>
+                                    <div className='subsubtitle'>Grid Lines</div>
+                                        <img  className={gridline===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>
                                 <div className={gridline===true ? 'content show':'content'} >{renderAllTableContent(props.data,"gridline")}
                                 </div>
@@ -125,8 +136,8 @@ export const WbTable = (props) =>{
                             <div>
                                 <div onClick={()=>{setZerolineClicked(prev=>!prev)}}
                                     className='table--sub--subtitle'>
-                                    Zero Lines
-                                    <div>{zeroline===false ? "+":"-"}</div>
+                                    <div className='subsubtitle'>Zero Lines</div>
+                                        <img  className={zeroline===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>
                                 <div className={zeroline===true ? 'content show':'content'} >{renderAllTableContent(props.data,"zeroline")}
                                 </div>
@@ -134,8 +145,8 @@ export const WbTable = (props) =>{
                             <div>
                                 <div onClick={()=>{setTrendlineClicked(prev=>!prev)}}
                                     className='table--sub--subtitle'>
-                                    Trend Lines
-                                    <div>{trendline===false ? "+":"-"}</div>
+                                    <div className='subsubtitle'>Trend Lines</div>
+                                        <img  className={trendline===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>
                                 <div className={trendline===true ? 'content show':'content'} >{renderAllTableContent(props.data,"trendline")}
                                 </div>
@@ -145,8 +156,8 @@ export const WbTable = (props) =>{
                             <div>
                                 <div onClick={()=>{setReflineClicked(prev=>!prev)}}
                                     className='table--sub--subtitle'>
-                                    Ref Lines
-                                    <div>{refline===false ? "+":"-"}</div>
+                                    <div className='subsubtitle'>Ref Lines</div>
+                                        <img  className={refline===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>
                                 <div className={refline===true ? 'content show':'content'} >{renderAllTableContent(props.data,"refline")}
                                 </div>
@@ -155,8 +166,8 @@ export const WbTable = (props) =>{
                             <div>
                                 <div onClick={()=>{setDroplineClicked(prev=>!prev)}}
                                     className='table--sub--subtitle'>
-                                    Drop Lines
-                                    <div>{dropline===false ? "+":"-"}</div>
+                                    <div className='subsubtitle'>Drop Lines</div>
+                                        <img  className={dropline===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>
                                 <div className={dropline===true ? 'content show':'content'} >{renderAllTableContent(props.data,"dropline")}
                                 </div>
@@ -165,8 +176,8 @@ export const WbTable = (props) =>{
                             <div>
                                 <div onClick={()=>{setAxisClicked(prev=>!prev)}}
                                     className='table--sub--subtitle'>
-                                    Axis
-                                    <div>{axis===false ? "+":"-"}</div>
+                                    <div className='subsubtitle'>Axis</div>
+                                        <img  className={axis===false ? 'chevron':'chevron open'} src={Chevron} ></img>
                                 </div>
 
                                 <div className={axis===true ? 'content show':'content'} >{renderAllTableContent(props.data,"axis")}
